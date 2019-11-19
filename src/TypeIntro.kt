c4d4e7cdf911027ea0c4724030f2ac7fa6e890ef
n-gregory-a <ovonely@gmail.com>
2019-11-19 09:22:01 +0700

    - add some more variables and values of different types - add outputs of this variables and values - reverse string with reversed() standard function

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -3,7 +3,19 @@ const val MAX_EXPERIENCE: Int = 5000
 fun main() {
     val playerName = "Estragon"
     var experiencePoints = 5
+
+    var hasSteed = false
+    var gold = 50
+    val tavernName = "Unicorn horn"
+    var beverages: List<String> = listOf("mead", "wine", "bear")
+
     experiencePoints += 5
-    println(experiencePoints)
-    println(playerName)
+    println("Experience points: $experiencePoints")
+    println("Player name is $playerName")
+
+    println("Player has a horse: $hasSteed")
+    println("Player has $gold gold")
+    println("Player in $tavernName tavern")
+
+    println("Mirror: " + playerName.reversed())
 }
