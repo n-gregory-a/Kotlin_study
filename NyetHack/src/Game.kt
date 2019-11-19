8206921d6327df8912fb9c16cae337eceb15292e
n-gregory-a <ovonely@gmail.com>
2019-11-19 14:35:14 +0700

    - add two new variables type of boolean - add "aura-is-visible" if/else condition - add if/else condition in one of levels of the else/if cascade

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -1,13 +1,26 @@
 fun main() {
     val name = "Madrigal"
-    val healthPoints = 89
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    // aura
+    if (isBlessed && healthPoints > 50 || isImmortal) {
+        println("GREEN")
+    } else {
+        println("NONE")
+    }
 
     if  (healthPoints == 100) {
         println(name + " is in exelent condition!")
     } else if (healthPoints >= 90) {
         println(name + " has a few scratches.")
     } else if (healthPoints >= 75) {
-        println(name + " has a minor wounds.")
+        if (isBlessed) {
+            println(name + " has some minor wounds but is healing quite quilckly!")
+        } else {
+            println(name + " has a minor wounds.")
+        }
     } else if (healthPoints >= 15) {
         println(name + " looks pretty hurt.")
     } else {
