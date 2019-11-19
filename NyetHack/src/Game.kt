43f8c1eaf2014e9c07ce02acf9b9b1b0fe705c3a
n-gregory-a <ovonely@gmail.com>
2019-11-19 15:02:40 +0700

    - refactoring by using conditional expressions

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -1,30 +1,29 @@
 fun main() {
     val name = "Madrigal"
-    var healthPoints = 89
+    val healthPoints = 89
     val isBlessed = true
     val isImmortal = false
 
     // aura
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    if (auraVisible) {
-        println("GREEN")
-    } else {
-        println("NONE")
-    }
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
 
-    if  (healthPoints == 100) {
-        println(name + " is in exelent condition!")
+    val healthStatus = if  (healthPoints == 100) {
+        " is in excellent condition!"
     } else if (healthPoints >= 90) {
-        println(name + " has a few scratches.")
+        " has a few scratches."
     } else if (healthPoints >= 75) {
         if (isBlessed) {
-            println(name + " has some minor wounds but is healing quite quilckly!")
+           " has some minor wounds but is healing quite quickly!"
         } else {
-            println(name + " has a minor wounds.")
+            " has a minor wounds."
         }
     } else if (healthPoints >= 15) {
-        println(name + " looks pretty hurt.")
+        " looks pretty hurt."
     } else {
-        println(name + " is in awful condition!")
+        " is in awful condition!"
     }
+    // player state
+    println(name + " " +healthStatus)
 }
