7ad6771c6fa042818c054e2a02faf649b137de87
n-gregory-a <ovonely@gmail.com>
2019-12-11 11:59:11 +0700

    function with lambda parameter

--- SandBox/src/SimVillage.kt
+++ SandBox/src/SimVillage.kt
@@ -1,10 +1,15 @@
 fun main() {
-    val greetingFunction: (String, Int) -> String = {playerName, numBuildings ->
+    val greetingFunction = {playerName:String, numBuildings: Int ->
         val currentYear = 2019
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
 
     }
 
-    println(greetingFunction("Guyal", 2))
+    runSimulation("Guyal", greetingFunction)
+}
+
+fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last()
+    println(greetingFunction(playerName, numBuildings))
 }
