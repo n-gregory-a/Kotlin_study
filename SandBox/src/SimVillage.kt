09788c51526648c38e9ab2e4e27fb357074f54ab
n-gregory-a <ovonely@gmail.com>
2019-12-12 11:41:43 +0700

    passing a function reference

--- SandBox/src/SimVillage.kt
+++ SandBox/src/SimVillage.kt
@@ -1,5 +1,5 @@
 fun main() {
-    runSimulation("Guyal") {playerName, numBuildings ->
+    runSimulation("Guyal", ::printConstructionCost) {playerName, numBuildings ->
         val currentYear = 2019
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
@@ -8,7 +8,15 @@ fun main() {
 
 }
 
-inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runSimulation(playerName: String,
+                         costPrinter: (Int) -> Unit,
+                         greetingFunction: (String, Int) -> String) {
     val numBuildings = (1..3).shuffled().last() // randomly choose 1, 2 or 3
+    costPrinter(numBuildings)
     println(greetingFunction(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuilding: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuilding}")
 }
