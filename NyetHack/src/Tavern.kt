2be4a1127db2be022cc77412aed4edc20713f648
n-gregory-a <ovonely@gmail.com>
2019-12-18 07:44:20 +0700

    using ?. operator

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -6,7 +6,14 @@ fun main() {
 //            "Buttered Ale"
 //        }
 //    }
-    var beverage = readLine()!!.capitalize()
+    var beverage = readLine()
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
+    }
+
+    val beverageServed: String = beverage ?: "Buttered Ale"
 //    beverage = null
     println(beverage)
 }
