b970464ed44e1a71dc4ad44f49479098fc8447a1
n-gregory-a <ovonely@gmail.com>
2019-12-18 07:33:24 +0700

    using !! operator

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,11 +1,12 @@
 fun main() {
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "Buttered Ale"
-        }
-    }
+//    var beverage = readLine()?.let {
+//        if (it.isNotBlank()) {
+//            it.capitalize()
+//        } else {
+//            "Buttered Ale"
+//        }
+//    }
+    var beverage = readLine()!!.capitalize()
 //    beverage = null
     println(beverage)
 }
