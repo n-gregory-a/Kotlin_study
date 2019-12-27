025d2de9c7ab90eda9251b58e31ece5fcf9f44ad
n-gregory-a <ovonely@gmail.com>
2019-12-27 07:49:14 +0700

    Strings: - modifying Tavern.kt - using split() function

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -2,12 +2,19 @@ const val TAVERN_NAME = "Taernyl's Folly"
 
 fun main() {
 
-    placeOrder()
+    placeOrder("shandy,Dragon's Breath,5.91")
 
 }
 
-fun placeOrder() {
+fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speak with $tavernMaster about his order.")
+
+    val data = menuData.split(',')
+    val type = data[0]
+    val name = data[1]
+    val price = data[2]
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
 }
