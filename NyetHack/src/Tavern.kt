dcc09428749bc4eb9816b4ad038fbd76a996cc1a
n-gregory-a <ovonely@gmail.com>
2019-12-30 09:45:32 +0700

    Strings: - modifying Tavern.kt - using replace()

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -6,6 +6,18 @@ fun main() {
 
 }
 
+private fun toDragonSpeak(phrase: String) =
+        phrase.replace(Regex("[aeiou]")) {
+            when (it.value) {
+                "a" -> "4"
+                "e" -> "3"
+                "i" -> "1"
+                "o" -> "0"
+                "u" -> "|_|"
+                else -> it.value
+            }
+        }
+
 fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
@@ -14,4 +26,7 @@ fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "Madrigal buys a $name ($type) for $price."
     println(message)
+
+    val phrase = "Ah, delicious $name!"
+    println("Mardigal exclaims: ${toDragonSpeak(phrase)}")
 }
