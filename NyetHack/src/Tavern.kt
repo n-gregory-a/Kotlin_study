86571acacd99dcdb8b11638cba17525af0eee2af
n-gregory-a <ovonely@gmail.com>
2020-01-04 08:39:43 +0700

    Numbers:   - using Double values   - converting Int to Double   - formatting Double value

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -12,7 +12,12 @@ fun main() {
 
 fun performPurchase(price: Double) {
     displayBalance()
+    val totalPurse = playerGold + (playerSilver / 100.0)
+    println("Total purse: $totalPurse")
     println("Purchasing item for $price")
+
+    val remainingBalance = totalPurse - price
+    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
 }
 
 fun displayBalance() {
